ab546b546dbfdf3a5579a70f723d9d54ae516bc9
julianhatwell <julian.hatwell@googlemail.com>
2016-01-18 08:03:58 +0800

    getting boiler plate code going

--- Code Snippets.R
+++ Code Snippets.R
@@ -4,34 +4,45 @@ library(ggplot2)
 library(parallel)
 library(doParallel)
 
-# f_classe <- factor(training_set$classe)
-# myScatterPlot <- function(j, df) {
-#   xyplot(df[[j]]~I(1:nrow(df))
-#          , groups = f_classe 
-#          , col = myPal
-#          , ylab = j
-#          , xlab = "index"
-#   )
-# }
-# 
-# myViolinPlot <- function(j, df) {
-#   bwplot(df[[j]]~f_classe
-#          , groups = f_classe
-#          , col = myPal
-#          , scales = list(y = list(tck = c(1, 0)))
-#          , panel = panel.superpose
-#          , panel.groups = panel.violin
-#          , ylab = j)
-# }
-# 
-# layoutPlots_4 <- function(vars, plotFunc, df) {
-#   print(plotFunc(vars[1], df), pos = c(0,0.5, 0.5, 1), more = TRUE)
-#   print(plotFunc(vars[2], df), pos = c(0.5, 0.5, 1, 1), more = TRUE)
-#   print(plotFunc(vars[3], df), pos = c(0, 0, 0.5, 0.5), more = TRUE)
-#   print(plotFunc(vars[4], df), pos = c(0.5, 0, 1, 0.5))
-# }
-# 
-# 
+# a nice colour palette
+myPal <- c("#8DD3C7", "#B0A8B3", "#9FB5D6", "#9EC0FA", "#DB8072")
+myPalDark <- c("#4D8377", "#504853", "#3F5576", "#3E409A", "#7B2012")
+myPalContrasts <- c(myPalDark[1], myPalDark[5], myPal[2], myPal[4]
+                    ,"#999999"
+                    , myPal[1], myPal[5], myPalDark[2], myPalDark[4])
+myPal.range <- colorRampPalette(c("#FFFFFF", myPal[3:1]))
+myPal.rangeDark <- colorRampPalette(c("#FFFFFF", myPalDark[3:1]))
+myPal.rangeDiv <- colorRampPalette(c(myPal[1], "#FFFFFF", myPal[5]))
+myPal.rangeContrasts <- colorRampPalette(c("#FFFFFF", myPalContrasts))
+
+myScatterPlot <- function(j, df) {
+  xyplot(df[[j]]~I(1:nrow(df))
+         , groups = y_fac 
+         , col = myPal.range(num_classes)
+         , alpha = 1/(length(training_ids)/2000)
+         , ylab = j
+         , xlab = "index"
+  )
+}
+
+myViolinPlot <- function(j, df) {
+  bwplot(df[[j]]~y_fac
+         , groups = y_fac
+         , col = myPal.rangeContrasts(num_classes)
+         , scales = list(y = list(tck = c(1, 0)))
+         , panel = panel.superpose
+         , panel.groups = panel.violin
+         , ylab = y)
+}
+
+layoutPlots_4 <- function(vars, plotFunc, df) {
+  print(plotFunc(vars[1], df), pos = c(0,0.5, 0.5, 1), more = TRUE)
+  print(plotFunc(vars[2], df), pos = c(0.5, 0.5, 1, 1), more = TRUE)
+  print(plotFunc(vars[3], df), pos = c(0, 0, 0.5, 0.5), more = TRUE)
+  print(plotFunc(vars[4], df), pos = c(0.5, 0, 1, 0.5))
+}
+
+
 # layoutPlots_4(exampleVars, myViolinPlot, training_set)
 
 
--- utilityCode.R
+++ utilityCode.R
@@ -1,13 +1,40 @@
+library(ggplot2)
+library(caret)
 data("diamonds")
 # to be generic
 df <- diamonds
-y <- "cut"
+resp <- "cut"
+# y_fac <- factor(df[[y]])
+resp_fac <- df[[y]]
+num_classes <- length(levels(resp_fac))
+
+getRespCol <- function(df, y) {
+  which(names(df) == y)
+}
+
+respCol <- getRespCol(df, y)
+vars <- names(df)[-respCol]
+
+#create or set factors
+vars_fac <- sapply(df, function(j) { any(class(j) == "factor") } )
+
+for (var in vars[!vars_fac]) {
+  v <- myViolinPlot(var, df)
+  print(v)
+}
 
 set.seed(23)
-training_ids <- createDataPartition(df[[y]], p = 0.6, list = FALSE)
+training_ids <- createDataPartition(df[[resp]], p = 0.6, list = FALSE)
 training_set <- df[training_ids,]
 
 holdout_set <- df[-training_ids,]
-validation_ids <- createDataPartition(holdout_set[[y]], p = 0.5, list = FALSE)
+validation_ids <- createDataPartition(holdout_set[[resp]], p = 0.5, list = FALSE)
 validation_set <- holdout_set[validation_ids, ]
 test_set <- holdout_set[-validation_ids, ]
+
+naVals_check <- apply(df, 2, function(i) { sum(is.na(i))} )
+
+summary(training_set)
+
+training_facsAsDummies <- dummyVars(resp~.,data = df)
+head(predict(training_facsAsDummies, newdata = df))
