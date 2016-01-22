7badcfaa874fb7e7ae43952206c83e6408e282b1
julianhatwell <julian.hatwell@googlemail.com>
2016-01-22 12:40:53 +0800

    imprvoved tc 3

---
+++ tuneGrids.R
@@ -0,0 +1,10 @@
+# tuneGrid examples
+
+gbmGrid <-  expand.grid(interaction.depth = c(1, 5, 9),
+                        n.trees = (1:30)*50,
+                        shrinkage = 0.1,
+                        n.minobsinnode = 20)
+
+brnn.grid <- expand.grid(neurons = c(3, 4, 5))
+
+lasso.grid <-  expand.grid(fraction = c(1, 5, 9))
