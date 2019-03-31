5bd0efd7b5bc1e1ad8bd6186bd4996f7f7b03eb7
Julian Hatwell <julianhatwell@users.noreply.github.com>
2019-03-31 19:44:37 +0100

    started ch 7

---
+++ linmods7.R
@@ -0,0 +1,37 @@
+library(faraway)
+data("cars")
+plot(dist ~ speed, data = cars)
+lmod <- lm(dist ~ speed, data = cars)
+sumary(lmod)
+abline(lmod)
+
+lmod1 <- lm(dist ~ I(speed+rnorm(50)), data = cars)
+coef(lmod1)
+lmod2 <- lm(dist ~ I(speed+2*rnorm(50)), data = cars)
+coef(lmod1)
+lmod5 <- lm(dist ~ I(speed+5*rnorm(50)), data = cars)
+coef(lmod1)
+coef(lmod2)
+coef(lmod5)
+abline(lmod1, lty=2)
+abline(lmod2, lty=3)
+abline(lmod5, lty=4)
+
+# add variances at different levels, rep 1000 each
+vv <- rep(1:5/10, each=1000)
+slopes <- numeric(5000)
+for(i in seq_along(slopes)) {
+  slopes[i] <- lm(dist ~ I(speed+sqrt(vv[i])*rnorm(50)), data = cars)$coef[2]
+}
+
+betas <- (c(coef(lmod)[2]
+        , colMeans(matrix(slopes
+                          , nrow = 1000))))
+variances <- c(0,1:5/10) + 0.5
+plot(variances, betas
+     , xlim = c(0,1)
+     , ylim=c(3.86, 4)
+     )
+gv <- lm(betas~variances)
+coef(gv)
+points(0, gv$coef[1], pch=3)
