a174f6b915c203ba261792ee6513eeacf607aa6d
julianhatwell <julian.hatwell@googlemail.com>
2019-08-15 22:13:31 +0100

    working on ch 15

--- linmods15.R
+++ linmods15.R
@@ -1,3 +1,5 @@
+library(faraway)
+library(ggplot2)
 data("coagulation")
 coagulation
 plot(coag~diet, data = coagulation)
@@ -8,6 +10,9 @@ sumary(lmod)
 round(coef(lmod), 1)
 model.matrix(lmod)
 anova(lmod)
+
+aov1 <- aov(coag~diet, data = coagulation)
+summary(aov1)
 
 lmod <- lm(coag~diet-1, data = coagulation)
 sumary(lmod)
@@ -26,4 +31,32 @@ bartlett.test(coag~diet, data = coagulation)
 lmod <- lm(coag~diet, data = coagulation)
 summary(lmod)
 confint(lmod)
-5 + c(-1, 1) * qt(0.975, 20) * 1.53 # se of this coef
+5 + c(-1, 1) * qt(0.975, 20) * 1.53 # se of this coef, good for one test
+# manual calc of tukey's honest significant difference, the terms at the end relate to the sample sizes
+5 + c(-1, 1) * qtukey(0.95, 4, 20)/sqrt(2) * summary(lmod)$sigma * sqrt((1/4) + (1/6))
+# or easier
+(tci <- TukeyHSD(aov(coag~diet, data = coagulation)))
+plot(tci)
+
+
+data(jsp)
+jsp$mathcent <- jsp$math - mean(jsp$math)
+ggplot(data = jsp, aes(x = school, y = mathcent)) +
+  geom_boxplot() +
+  theme(axis.text.x = element_text(angle = 90)) +
+  ylab("Centered Math Scores")
+
+lmod <- lm(mathcent ~ school - 1, data = jsp)
+summary(lmod)
+anova(lmod)
+aov(mathcent ~ school - 1, data = jsp)
+
+# bonferroni
+pvals <- summary(lmod)$coef[, 4]
+padj <- p.adjust(pvals, method = "bonferroni")
+coef(lmod)[padj < 0.05]
+# benjamin and hochberg method, better for large numbers of comparisons
+names(which(sort(pvals) < 1:length(pvals)*0.05/length(pvals)))
+# more conveniently
+padj <- p.adjust(pvals, method = "fdr")
+coef(lmod)[padj < 0.05]
