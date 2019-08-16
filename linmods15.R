21b1a72ce1109518e6e67b7503dc5d0de6ecf75a
julianhatwell <julian.hatwell@googlemail.com>
2019-08-16 20:23:44 +0100

    working on linmods 15

--- linmods15.R
+++ linmods15.R
@@ -64,18 +64,28 @@ padj <- p.adjust(pvals, method = "fdr")
 coef(lmod)[padj < 0.05]
 
 data(pulp)
-names(pulp)
 plot(bright~operator, data = pulp)
 summary(aov(bright~operator, data = pulp))
 summary(lm(bright~operator, data = pulp))
 stripchart(bright~operator, data = pulp
            , vertical = TRUE, method = "stack")
-stripplot(jitter(bright)~operator, data = pulp
-           , vertical = TRUE, method = "stack")
+stripplot(jitter(bright)~operator, data = pulp)
 stripplot(bright~jitter(as.numeric(operator), 0.5), data = pulp
-          , vertical = TRUE, method = "stack"
           , xlab = "operator"
-          , scales = list(x = list(labels = c("a","a", "b", "c", "d"))))
+          , scales = list(x = list(
+            at = 1:4
+            , labels = c("a", "b", "c", "d")))
+)
+# bonferroni
+pvals <- summary(lmod)$coef[, 4]
+padj <- p.adjust(pvals, method = "bonferroni")
+coef(lmod)[padj < 0.05]
+# benjamin and hochberg method, better for large numbers of comparisons
+names(which(sort(pvals) < 1:length(pvals)*0.05/length(pvals)))
+# more conveniently
+padj <- p.adjust(pvals, method = "fdr")
+coef(lmod)[padj < 0.05]
+# with 4 choose 2 pairwise tests, none are signif
 
 data("chickwts")
 plot(weight~feed, data = chickwts)
@@ -97,3 +107,83 @@ anova(lm(ar~feed, data = chickwts)) # Levene's Test
 bartlett.test(weight~feed, data = chickwts)
 # to see if group variances are the same
 leveneTest(weight~feed, data = chickwts)
+
+data("PlantGrowth")
+plot(weight~group, data = PlantGrowth)
+stripchart(weight~group,data = PlantGrowth
+           , vertical = TRUE, method = "stack")
+stripplot(weight~jitter(as.numeric(group), 0.5), data = PlantGrowth
+          , xlab = "group"
+           , scales = list(x = list(
+             at = 1:3
+             , labels = c("ctrl", "trt1", "trt2")))
+)
+lmod <- lm(weight~group, data = PlantGrowth)
+lmnull <- lm(weight~1, data = PlantGrowth)
+anova(lmnull, lmod)
+summary(lmod)
+qqnorm(resid(lmod)) # skew?
+qqline(resid(lmod))
+plot(jitter(fitted(lmod)), resid(lmod)) # heterosked?
+# no evidence of non-constant variance
+bartlett.test(weight~group, data = PlantGrowth)
+# to see if group variances are the same - no evidence of diff
+leveneTest(weight~group, data = PlantGrowth)
+# abolute median resids (manual Levene's)
+med <- with(PlantGrowth, tapply(weight, group, median))
+ar <- with(PlantGrowth, abs(weight-med[group]))
+anova(lm(ar~group, data = PlantGrowth)) # same
+
+# family wise error
+pvals <- summary(lmod)$coef[, 4]
+padj <- p.adjust(pvals, method = "bonferroni")
+coef(lmod)[padj < 0.05]
+# benjamin and hochberg method, better for large numbers of comparisons
+names(which(sort(pvals) < 1:length(pvals)*0.05/length(pvals)))
+# more conveniently
+padj <- p.adjust(pvals, method = "fdr")
+coef(lmod)[padj < 0.05]
+# with 3 choose 2 pairwise tests, none are signif, but none were when unadjusted in any case
+
+PlantGrowth$cgrp <- factor(ifelse(PlantGrowth$group == "ctrl", "ctrl", "trt"))
+summary(lm(weight~cgrp, data = PlantGrowth))
+leveneTest(weight~cgrp, data = PlantGrowth)
+
+data("infmort")
+summary(aov(income~region, data = infmort))
+summary(lm(income~region, data = infmort))
+plot(income~region, data = infmort)
+plot(log(income)~region, data = infmort)
+summary(lm(log(income)~region, data = infmort))
+
+data("anaesthetic")
+plot(breath~tgrp, data = anaesthetic)
+stripchart(breath~tgrp, data = anaesthetic
+           , vertical = TRUE, method = "stack")
+stripplot(breath~jitter(as.numeric(tgrp), 0.5), data = anaesthetic
+          , xlab = "group"
+          , scales = list(x = list(
+            at = 1:4
+            , labels = c("A", "B", "C", "D")))
+)
+ggplot(data = anaesthetic
+       , aes(x = tgrp, y = breath)) +
+  geom_boxplot() +
+  geom_point(position = position_jitter(width = 0.1))
+summary(lm(breath~tgrp, data = anaesthetic))
+# box cox will fail because presence of zeros
+ggplot(data = anaesthetic
+       , aes(x = tgrp, y = sqrt(breath))) +
+  geom_boxplot() +
+  geom_point(position = position_jitter(width = 0.1))
+lmod <- lm(sqrt(breath)~tgrp, data = anaesthetic)
+summary(lmod)
+qqnorm(resid(lmod)) # not good
+qqline(resid(lmod)) 
+plot(jitter(fitted(lmod)), resid(lmod)) # heterosked?
+# no evidence of non-constant variance
+bartlett.test(breath~tgrp, data = anaesthetic) # not satisfactory
+# to see if group variances are the same - no evidence of diff
+leveneTest(breath~tgrp, data = anaesthetic)
+# Wikipedia says that Bartlett's test is more sensitive to violations of normality than Levene's test.
+# So you may have non-normal data instead of heteroscedastic data.
